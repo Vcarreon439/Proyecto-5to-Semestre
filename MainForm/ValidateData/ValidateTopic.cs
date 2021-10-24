b228499242db45af35e03fa0e53efaf28e7d1084
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2021-10-24 16:07:05 -0500

    DataSourceElementos para la validacion de los procedimientos relacionados a Tema. Clase ValidateTopic, para la validacion de los campos generados por el DataSource Elementos.Tema

---
+++ MainForm/ValidateData/ValidateTopic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elementos;
+using FluentValidation;
+
+namespace MainForm.ValidateData
+{
+    /// <summary>
+    /// Clase para la validacion de campos de Tema
+    /// </summary>
+    public class ValidateTopic : AbstractValidator<Tema>
+    {
+        /// <summary>
+        /// Establece que los campos Codigo y Descripcion no pueden estar vacios
+        /// </summary>
+        public ValidateTopic()
+        {
+            RuleFor(x => x.Codigo).NotEmpty();
+            RuleFor(x => x.Descripcion).NotEmpty();
+        }
+    }
+}
